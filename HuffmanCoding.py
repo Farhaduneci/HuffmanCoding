02f059193a0f902f9f3e496ea49313254e7a5853
Farhad <farhaduneci@gmail.com>
2021-10-02 13:47:12 +0330

    Code Upload

--- HuffmanCoding.py
+++ HuffmanCoding.py
@@ -1,3 +1,7 @@
+from queue import PriorityQueue
+import json
+
+
 class Node:
     def __init__(self, symbol, freq, left=None, right=None):
         # symbol
@@ -12,5 +16,80 @@ class Node:
         # right node
         self.right = right
 
-        # tree direction (0/1)
-        self.code = ''
+    def __repr__(self):
+        return self.symbol.upper() + str(self.freq)
+
+    def __lt__(self, other):
+        return self.freq < other.freq
+
+    def __gt__(self, other):
+        return self.freq > other.freq
+
+
+def calcCharFrequencies(data):
+    symbols = dict()
+    for element in data:
+        if symbols.get(element) == None:
+            symbols[element] = 1
+        else:
+            symbols[element] += 1
+    return symbols
+
+
+def calcCodes(node, code='', codes=dict()):
+    if(node.left):
+        calcCodes(node.left, code + '0', codes)
+    if(node.right):
+        calcCodes(node.right, code + '1', codes)
+
+    if(not node.left and not node.right):
+        codes[node.symbol] = code
+
+    return codes
+
+
+def encode(data):
+    analytics = calcCharFrequencies(data)
+    symbols = analytics.keys()
+
+    nodes = PriorityQueue()
+
+    # converting symbols and frequencies into huffman tree nodes
+    for symbol in symbols:
+        nodes.put(Node(symbol, analytics.get(symbol)))
+
+    while nodes.qsize() > 1:
+        # pick 2 smallest nodes
+        left, right = nodes.get(), nodes.get()
+
+        # combine the 2 smallest nodes to create new node
+        nodes.put(Node(left.symbol + right.symbol,
+                       left.freq + right.freq, left, right))
+
+    # extract huffman tree from priority queue
+    huffmanTree = nodes.get()
+
+    # calculate encoding code for each character
+    codes = calcCodes(huffmanTree)
+
+    print(json.dumps(codes, indent=1))
+
+    encodedData = ''
+    for char in data:
+        encodedData += codes.get(char)
+
+    # return the encoded output & the huffman tree for future decoding
+    return encodedData, huffmanTree
+
+
+def decode(encodedData, huffmanTree):
+    currentNode = huffmanTree
+    decodedData = ''
+
+    for bit in encodedData:
+        currentNode = currentNode.right if int(bit) == 1 else currentNode.left
+        if currentNode.left == None and currentNode.right == None:
+            decodedData += currentNode.symbol
+            currentNode = huffmanTree
+
+    return decodedData
